b24f49342fcbb86205d001605d7e54cef35c76c0
奉伟 <landwind1180@gmail.com>
2021-06-01 16:52:40 +0800

    update config

---
+++ Hei.Api/run.bat
@@ -0,0 +1,2 @@
+dotnet build
+dotnet bin\Debug\netcoreapp3.1\Hei.Api.dll --urls http://127.0.0.1:5003
---
+++ Hei.IdentityServer/run.bat
@@ -0,0 +1,2 @@
+dotnet build
+dotnet bin\Debug\netcoreapp3.1\Hei.IdentityServer.dll --urls http://127.0.0.1:5100
